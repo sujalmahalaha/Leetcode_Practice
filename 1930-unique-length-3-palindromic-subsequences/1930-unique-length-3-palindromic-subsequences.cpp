6ffab156c7b888394e0667dc7465f146539b88f2
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2025-01-04 20:01:29 +0530

    Time: 171 ms (65.65%), Space: 15.9 MB (69.02%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_set<char> letters;
+        int n = s.size();
+        for (auto ch : s) {
+            letters.insert(ch);
+        }
+        int result = 0;
+        for (auto letter : letters) {
+            int left = -1;
+            int right = -1;
+            for (int i = 0; i < n; i++) {
+                if (s[i] == letter) {
+                    if (left == -1) {
+                        left = i;
+                    }
+                    right = i;
+                }
+            }
+            unordered_set<char> st;
+            for (int middle = left+1; middle <= right-1; middle++) {
+                st.insert(s[middle]);
+            }
+            result += st.size();
+        }
+        return result;
+    }
+};
