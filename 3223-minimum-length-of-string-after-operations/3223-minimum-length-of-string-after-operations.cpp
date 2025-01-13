c510cd2968d7239111e47434ff33d30594058315
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2025-01-13 22:41:12 +0530

    Time: 29 ms (58.47%), Space: 32.1 MB (58.06%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.size();
+        int deleted = 0;
         vector<int>freq(26,0);
         for(int i = 0 ; i<n; i++){
             freq[s[i]-'a']++;
             if(freq[s[i]-'a'] >=3){
                 freq[s[i]-'a'] -= 2;
+                deleted += 2;
             }
         }
-        int result = 0;
-
-         for(int i = 0 ; i<26 ; i++){
-           result += freq[i];
-        }
-        return result;
+        return  n-deleted;
     }
    
 };
