bfc39ecca0cd207fc5e5fbf8367a6d9998ed49ba
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2025-01-14 11:19:57 +0530

    Time: 13 ms (37.37%), Space: 90.7 MB (16.41%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        vector<int>result(n);
-        int count ;
-        for(int i = 0; i<n ; i++){
-             count = 0;
-            for(int j = 0 ; j<=i ; j++){
-             for(int k = 0 ; k<=i; k++){
-                if(A[j]==B[k]){
-                    count++;
-                }
-             }
-            }
-            result[i] = count; 
+   vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+    int n = A.size();
+    vector<int> result(n, 0);
+    unordered_set<int> seenA, seenB;
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        // Add current elements of A and B to their respective sets
+        if (seenB.count(A[i])) {
+            ++count;  // A[i] is already in seenB, so it's common
         }
-        return result;
-        
+        seenA.insert(A[i]);
+
+        if (seenA.count(B[i])) {
+            ++count;  // B[i] is already in seenA, so it's common
+        }
+        seenB.insert(B[i]);
+
+        // Update the result for the current index
+        result[i] = count;
     }
+
+    return result;
+}
+
 };
